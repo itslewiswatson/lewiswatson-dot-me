19e21eece0adea1db4536ded050b5f16e09ac21c
Lewis Watson <noki@zorque.xyz>
2021-06-14 15:29:07 +1000

    Added contact icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
 import { theme } from "./common/theme";
+import { GrLinkedinOption, GrGithub, GrMail } from "react-icons/gr";
 
 const App = () => {
   const classes = useStyles();
@@ -27,6 +28,26 @@ const App = () => {
             where I focus on designing and building brilliant products for
             forward-thinking organisations.
           </p>
+        </div>
+
+        <div className={classes.iconBar}>
+          <a
+            href="https://linkedin.com/in/itslewiswatson"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrLinkedinOption className={classes.icon} size={28} />
+          </a>
+          <a
+            href="https://github.com/itslewiswatson"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrGithub className={classes.icon} size={28} />
+          </a>
+          <a href="mailto:hello@lewiswatson.me">
+            <GrMail className={classes.icon} size={28} />
+          </a>
         </div>
       </div>
     </div>
@@ -106,6 +127,41 @@ const useStyles = createUseStyles({
     color: theme.colors.dark,
     maxWidth: "600px",
     fontSize: 20,
+    marginBottom: 50,
+
+    "@media (prefers-color-scheme: dark)": {
+      color: theme.colors.offWhite,
+    },
+
+    "@media (prefers-color-scheme: light)": {
+      color: theme.colors.dark,
+    },
+  },
+
+  iconBar: {
+    display: "flex",
+    justifyContent: "center",
+    gap: 10,
+  },
+
+  icon: {
+    borderRadius: 10,
+    color: theme.colors.primary,
+    backgroundColor: theme.colors.dark,
+
+    "@media (prefers-color-scheme: dark)": {
+      color: theme.colors.primary,
+      backgroundColor: theme.colors.dark,
+    },
+
+    "@media (prefers-color-scheme: light)": {
+      color: theme.colors.primary,
+      backgroundColor: theme.colors.offWhite,
+    },
+
+    "&:hover": {
+      cursor: "pointer",
+    },
   },
 });
 
